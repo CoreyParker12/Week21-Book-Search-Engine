1dc173bb4c52715fc28d07be8977a7162f7ff7d7
Corey Parker <coreyparker012@gmail.com>
2021-09-26 18:49:26 -0400

    error fixing in app.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -4,6 +4,8 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import {setContext} from '@apollo/client/link/context';
+
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink  } from '@apollo/client';
 
 const httpLink = createHttpLink({
