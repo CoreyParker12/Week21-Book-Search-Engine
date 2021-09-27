8ab19089437bf5fabb42afb9584c4b0153e76401
Corey Parker <coreyparker012@gmail.com>
2021-09-26 21:24:25 -0400

    testing to see if upload works. Having issues

---
+++ client/build/precache-manifest.086acb66dd152124348161e44ee7f7df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e191eaba4cafda7f61ec4a32d2f687f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e9b8e5586dbebd44fa2",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "48fecef55ded42d4e1d2",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "0e9b8e5586dbebd44fa2",
+    "url": "/static/js/2.d53f8083.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.d53f8083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fecef55ded42d4e1d2",
+    "url": "/static/js/main.57aa9141.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
